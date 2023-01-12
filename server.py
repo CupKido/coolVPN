dcf21b67ba30def0b6e562f91fd41fcdb9fb97e1
saartaler <talersaar@gmail.com>
2023-01-12 18:28:55 +0200

    comments for server

--- server.py
+++ server.py
@@ -15,13 +15,23 @@ REAL_INTERFACE = "MediaTek Wi-Fi 6 MT7921 Wireless LAN Card"
 LOOPBACK_INTERFACE = "Software Loopback Interface 1"
 SERVER_PORT = 6493
 RSA_KEYS = ()
+
+
 def StartServer():
+    '''
+    Starts the server
+    '''
     global CLIENT_INTERFACE, SERVER_PORT, REAL_INTERFACE
     confirm_keys()
+    # sniffs the packets and sends them to be processed
     sniff(iface=REAL_INTERFACE,  prn=ProcessPackets)
     return
 
+
 def confirm_keys():
+    '''
+    Checks if the keys exist, if not, generates them
+    '''
     global RSA_KEYS
     try:
         with open('server_keys.bin', 'rb') as f:
@@ -35,29 +45,39 @@ def confirm_keys():
     RSA_KEYS = key_set
 
 def ProcessPackets(pkt):
+    '''
+    checks packet contents and takes care of it in one of 3 ways:
+    1. the packet is a request for the public key, so it sends it
+    2. the packet is a request for a connection, so it generates an ID, and asymetric key 
+       and sends it encrypted with the client's public key
+    3. the client already exists, so it changes packet's source to the server's IP and forwards it
+    '''
+    # assuming client sport is 6494
     if not (TCP in pkt and pkt[TCP].dport == SERVER_PORT and pkt[TCP].sport==6494):
         #print("caught useless")
         return
-    print("caught")
+    print("caught relevant package")
+    #check if client already exists, and process
     if IP in pkt and pkt[IP].src in Connected_Client.keys():
         process_and_forward(pkt, pkt[IP].src)
         return
+    #check if packet has content
     if pkt.haslayer(Raw):
+        #check if packet is a request for public key
         if pkt.getlayer(Raw).load == b'Get Public Key':
             send_public_key(pkt)
             return
+        #check if packet is a request for a connection
         raw_data = pkt.getlayer(Raw).load
         data = pickle.loads(rsa.decrypt(raw_data, RSA_KEYS[1]))
         print(data)
         if(data == "StartConnection"):
            GenerateAndSendID(pkt, data)
-        elif(data[0] in Connected_Client.keys()):
-           packet = data[1]
-           packet[IP].src = IP().src
-           packet.display()
-           send(packet)
 
 def process_and_forward(pkt, client_ip):
+    '''
+    decryptes clients package, changes the source to the server's IP, and forwards it
+    '''
     if pkt.haslayer(Raw):
         symmetric_key = Connected_Client[client_ip][1]
         fernet = Fernet(symmetric_key)
@@ -72,6 +92,9 @@ def process_and_forward(pkt, client_ip):
 
     
 def send_public_key(pkt):
+    '''
+    sends the public key to the packet's source
+    '''
     global RSA_KEYS
     raw_data = RSA_KEYS[0]
     packet = IP(dst=pkt[IP].src) / TCP(dport=pkt[TCP].sport, sport=SERVER_PORT) / get_raw_of(raw_data)
@@ -80,6 +103,9 @@ def send_public_key(pkt):
 
 
 def GenerateAndSendID(original_pkt, data):
+    '''
+    Generates an ID, and asymetric key and sends it encrypted with the client's public key
+    '''
     global SERVER_PORT, CLIENT_INTERFACE, REAL_INTERFACE
     p = sniff(count=1, lfilter= lambda x: IP in x and x[IP].src == original_pkt[IP].src and TCP in x and x[TCP].dport == SERVER_PORT, timeout=3)
     if not p:
@@ -88,29 +114,40 @@ def GenerateAndSendID(original_pkt, data):
     if key_pkt.haslayer(Raw):
         global MY_ID
         data = key_pkt.getlayer(Raw).load
+        # decrypting the client's public key
         client_public_key = pickle.loads(rsa.decrypt(data, RSA_KEYS[1]))
         print("clients public = " + str(client_public_key))
+        # generating a new ID and symmetric key, and saving them in the clients dictionary based on client's IP
         new_client_data = (GenerateID(), GenerateSymmetric())
         Connected_Client[original_pkt[IP].src] = new_client_data
+        # sending the ID
         id_packet = IP(dst=original_pkt[IP].src) / TCP(dport=original_pkt[TCP].sport, sport=SERVER_PORT) / get_raw_RSAencrypted_of(new_client_data[0], client_public_key)
         id_packet.display()
         send(id_packet, iface=REAL_INTERFACE)
         print(str(new_client_data))
+        # splits the symmetric key to three parts
         bytes_packet = pickle.dumps(new_client_data[1])
         third = int(len(bytes_packet) / 3)
         parts = (bytes_packet[:third], bytes_packet[third:2*third], bytes_packet[2*third:])
+        # sends the symmetric key parts
+        # part 1
         key_packet1 = IP(dst=original_pkt[IP].src) / TCP(dport=original_pkt[TCP].sport, sport=SERVER_PORT) / Raw(rsa.encrypt(parts[0],client_public_key))
         key_packet1.display()
         send(key_packet1, iface=REAL_INTERFACE)
+        # part 2
         key_packet2 = IP(dst=original_pkt[IP].src) / TCP(dport=original_pkt[TCP].sport, sport=SERVER_PORT) / Raw(rsa.encrypt(parts[1],client_public_key))
         key_packet2.display()
         send(key_packet2, iface=REAL_INTERFACE)
+        # part 3
         key_packet3 = IP(dst=original_pkt[IP].src) / TCP(dport=original_pkt[TCP].sport, sport=SERVER_PORT) / Raw(rsa.encrypt(parts[2],client_public_key))
         key_packet3.display()
         send(key_packet3, iface=REAL_INTERFACE)
     
 
 def GenerateID():
+    '''
+    Generates a random ID with 5 digits
+    '''
     r = random
     res = ""
     for x in range(5):
@@ -118,17 +155,27 @@ def GenerateID():
     return res
 
 def GenerateSymmetric():
-    #Generate AES Key
+    '''
+    Generates a symmetric key
+    '''
     return Fernet.generate_key()
 
 def get_raw_RSAencrypted_of(data, PubKey):
+    '''
+    returns a raw packets with the data encrypted with the public key as it's content
+    '''
     return Raw(rsa.encrypt(pickle.dumps(data), PubKey))
 
 def get_raw_AESencrypted_of(ID, data): 
+    '''
+    returns a raw packets with the data encrypted with the symmetric as it's content
+    '''
     global Connected_Client
     key = Connected_Client[ID]
     return Raw(Fernet.encrypt(pickle.dumps(data)))
 
 def get_raw_of(data): return Raw(pickle.dumps(data))
 
+
+# Main
 StartServer()
