f55a551e6e2f677c87395ab14219cfe5575db70e
Shoham Zeharya <shozeh123@gmail.com>
2023-01-17 22:15:40 +0200

    client receives ping packets

--- ping.py
+++ ping.py
@@ -4,14 +4,16 @@ import sys
 
 
 def ping(ip, iface, count):
-    ip = IP(dst=ip)
-    icmp = ICMP()
-    ping_pkt = ip / icmp
+    iface="CoolVPN"
+    # ip = IP(src=get_if_addr(iface), dst=ip)
+    ip = IP(src="169.254.63.38", dst=ip)
+    icmp = ICMP(id=1, seq=1)
+    ping_pkt = ip / icmp / Raw(b'abcdefghijklmnopqrstuvwabcdefghi')
 
     while count > 0:
 
         # Send the ping packet and receive the reply
-        ping_response = sr1(ping_pkt, iface=iface, timeout=3)
+        ping_response = sr1(ping_pkt, iface=iface, timeout=10)
 
         # Print the reply if received
         if ping_response:
@@ -23,14 +25,18 @@ def ping(ip, iface, count):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 1:
         print('usage:\n     python ping.py -c count -t target_ip -i interface')
         exit()
 
     count = sys.argv[sys.argv.index("-c") + 1]
+    #count = "4"
+
     target_ip = sys.argv[sys.argv.index("-t") + 1]
+    #target_ip = "8.8.8.8"
 
     interface = sys.argv[sys.argv.index("-i") + 1]
+    #interface = "CoolVPN"
 
     try:
         count = int(count)
