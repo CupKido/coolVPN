a03343a8fd5caf0f1e8311bc3a1d51803dd5192b
saartaler <talersaar@gmail.com>
2023-01-18 13:53:12 +0200

    update server view

--- server.py
+++ server.py
@@ -72,6 +72,8 @@ def ProcessPackets(pkt):
     """
     global RSA_KEYS, SERVER_ADDRESS
     if not (pkt.haslayer(IP) and (pkt[IP].dst == '127.0.0.1' or pkt[IP].dst == SERVER_ADDRESS)):
+        if pkt.haslayer(TCP) and pkt[TCP].dport == 22:
+            return
         pkt.display()
         return
     if TCP in pkt:
